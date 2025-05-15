7ba7fad8c17342c453be56f3c2c99e66ba57dd35
Rafael Reynaq <rafael.reyna@metova.com>
2025-05-15 01:34:38 -0600

    FINTECH-10: Request tracking

---
+++ app/Http/Middleware/TrackRequestMiddleware.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Services\ExternalLogger\ExternalLoggerService;
+use \Illuminate\Http\Request;
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class TrackRequestMiddleware
+{
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        try {
+            $traceId = $request->headers->get(InjectLogTraceIDMiddleware::TRACE_ID_HEADER);
+            $service = new ExternalLoggerService($traceId, date('Y-m-d H:i:s'));
+
+            $service->createRequestEntry(
+                $request->method(),
+                $request->path(),
+                [
+                    'ip' => $request->ip(),
+                    'user_agent' => $request->header('User-Agent'),
+                    'referrer' => $request->header('Referer'),
+                    'user_id' => Auth::id() ?? null,
+                ],
+                $request->getquerystring(),
+                $request->post(),
+                $request->headers->all(),
+            );
+
+            $service->saveLog();
+        } catch (\Throwable $e) {
+            Log::error("No se pudo trackear este request" - $e->getMessage());
+        }
+
+        $response = $next($request);
+        return $response;
+    }
+}
